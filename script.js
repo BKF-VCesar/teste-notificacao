7a1069ba658108f35a1d9fcc4d75e5d99ec2eced
BKF-VCesar <inovacao2@breitkopf.com.br>
2023-02-17 10:39:49 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+let button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+  console.log("solicitando permissão...");
+  // Verificar se as notificações são suportadas pelo navegador
+  if (!("Notification" in window)) {
+    console.log("Este navegador não suporta notificações.");
+    alert("Este navegador não suporta notificações.");
+  } else {
+    // Verificar se a permissão ainda não foi concedida pelo usuário
+    if (Notification.permission === "granted") {
+      console.log("Permissão já concedida.");
+      alert("Você já concedeu permissão para receber notificações.");
+    } else if (Notification.permission !== "denied") {
+      // Solicitar permissão
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          console.log("Permissão concedida pelo usuário.");
+          alert("Você concedeu permissão para receber notificações.");
+        }
+      });
+    }
+  }
+});
